bb07ad1122e4eeb00f445f51dd589c29885061ec
Hasul79 <hatuk2@mail.ru>
2023-09-15 00:01:51 +0400

    add function

---
+++ main.js
@@ -0,0 +1,11 @@
+function rain(){
+    let cloud = document.querySelector(".cloud");
+    let e = document.createElement("div");
+
+    e.classList.add('drop');
+    cloud.appendChild(e);
+}
+setInterval(function(){
+ rain()
+},1000)
+
